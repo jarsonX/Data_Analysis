6e3bd26ff4175949a17e158633be902ac8e374e5
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-05-28 15:11:30 +0200

    Add files via upload

---
+++ Data-wrangling-04.py
@@ -0,0 +1,29 @@
+#Downloading And Scraping The Contents Of A Web Page
+
+#Scrape all links
+#Scrape all images Tags
+#Save results to a file
+
+import requests
+from bs4 import BeautifulSoup
+
+#Get and prepare data
+url = "http://www.ibm.com"
+data  = requests.get(url).text 
+
+soup = BeautifulSoup(data,"html.parser")
+
+#Save data
+write_file = open(r"C:\Users\krzys\Desktop\scrap_01.txt", 'w')
+
+print("Links", file=write_file)
+for link in soup.find_all('a',href=True):
+    print(link.get('href'), file=write_file)
+
+print("Images Tags", file=write_file)
+for link in soup.find_all('img'):
+    print(link, file=write_file)
+    print(link.get('src'), file=write_file)
+    
+write_file.close()
+print("Done")
