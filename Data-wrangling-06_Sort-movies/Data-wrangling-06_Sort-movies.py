20329a232e1c5858ad045183ead2409777f75f3c
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-05-30 20:20:37 +0200

    Update Data-wrangling-06_Sort-movies.py

--- Data-wrangling-06_Sort-movies/Data-wrangling-06_Sort-movies.py
+++ Data-wrangling-06_Sort-movies/Data-wrangling-06_Sort-movies.py
@@ -1,6 +1,5 @@
 #File 'top500' contains names and gross revenues of 500 films.
 #Those films need to be sorted based on revenues in descending order.
-
 import csv
 #This can be done without csv module but it will be easier that way.
 
@@ -11,17 +10,21 @@ def sort_films(input_file, output_file):
     #read the file
     with open(input_file, 'r') as inp:
         inp_reader = csv.reader(inp, delimiter = '\t')  #to avoid splitting
-        
-        #fill the movie_list
+        #csv.reader reads the file and returns an iterable object
+        #then thanks for the loop, each line is converted into a list
+        #and appended to the movie_list
         for line in inp_reader:
-            movie_list.append((line[0], int(line[1])))
+            movie_list.append((int(line[1]), (line[0])))
         
     #sort the list
-    movie_list_sorted = sorted(movie_list, key=lambda x: x[1], reverse=True)
-        
+    movie_list_sorted = sorted(movie_list, reverse=True)
+    
+    #Note: key parameter would be a good way to sort, if we wanted a different order
+    #movie_list_sorted = sorted(movie_list, key=lambda x: x[1], reverse=True)  
+    
     #save the list
     with open(output_file, 'w') as out:
         for movie in movie_list_sorted:
-            print(movie[1], '\t', movie[0], file=out)
+            print(movie[0], '\t', movie[1], file=out)
   
 sort_films('...\top500.txt', '...\top500result.txt')
